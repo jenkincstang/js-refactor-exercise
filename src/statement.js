2118e2fd58fc156bb9c7e85f0d568e4056b6bc88
jenkincstang <jenkincs@163.com>
2020-08-27 17:50:17 +0800

    [js-refactor][Jenkin] refactor: refactor statement() method

--- src/statement.js
+++ src/statement.js
@@ -47,19 +47,25 @@ function generateAmountOwedInfo(totalAmount, type) {
   return null;
 }
 
-function statement (invoice, plays) {
-  let totalAmount = 0;
+function calculateTotalVolumeCredits(invoice, plays){
   let volumeCredits = 0;
-  let result = generateCustomerInfo(invoice, "plain");
-
-  function calculateVolumeCredits(perf, play) {
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    return volumeCredits;
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    volumeCredits = calculateVolumeCredits(perf, play, volumeCredits);
   }
+  return volumeCredits;
+}
 
+function calculateVolumeCredits(perf, play, volumeCredits) {
+  // add volume credits
+  volumeCredits += Math.max(perf.audience - 30, 0);
+  // add extra credit for every ten comedy attendees
+  if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+  return volumeCredits;
+}
+
+function calculateTotalAmount(invoice, plays) {
+  let totalAmount = 0;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
@@ -73,28 +79,43 @@ function statement (invoice, plays) {
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
-    volumeCredits = calculateVolumeCredits(perf, play);
-    //print line for this order
-    result += generateOrderInfo(play, thisAmount, perf, "plain");
     totalAmount += thisAmount;
   }
-  result += generateAmountOwedInfo(totalAmount, "plain");
-  result += generateCreditsInfo(volumeCredits, "plain");
+  return totalAmount;
+}
+
+function generateTotalOrdersInfo(invoice, plays){
+  let result = "";
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+    switch (play.type) {
+      case 'tragedy':
+        thisAmount = calculateTragedyAmount(thisAmount, perf);
+        break;
+      case 'comedy':
+        thisAmount = calculateComedyAmount(thisAmount, perf);
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    //print line for this order
+    result += generateOrderInfo(play, thisAmount, perf, "plain");
+  }
+  return result;
+}
+
+function statement (invoice, plays) {
+  let result = generateCustomerInfo(invoice, "plain");
+  result += generateTotalOrdersInfo(invoice, plays);
+  result += generateAmountOwedInfo(calculateTotalAmount(invoice, plays), "plain");
+  result += generateCreditsInfo(calculateTotalVolumeCredits(invoice, plays), "plain");
   return result;
 }
 
 function statementHtml (invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = generateCustomerInfo(invoice, "html");
-
-  function calculateVolumeCredits(perf, play) {
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    return volumeCredits;
-  }
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
@@ -109,13 +130,12 @@ function statementHtml (invoice, plays) {
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
-    volumeCredits = calculateVolumeCredits(perf, play);
     //print line for this order
     result += generateOrderInfo(play, thisAmount, perf, "html");
     totalAmount += thisAmount;
   }
   result += generateAmountOwedInfo(totalAmount, "html");
-  result += generateCreditsInfo(volumeCredits, "html");
+  result += generateCreditsInfo(calculateTotalVolumeCredits(invoice, plays), "html");
   return result;
 }
 
